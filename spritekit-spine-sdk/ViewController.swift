3564fe984f44c26428b395ec3f872a1038e7f62b
Thiago Medeiros dos Santos <thiago.santos@movile.com>
2015-12-19 14:00:22 -0200

    Initial Commit

---
+++ spritekit-spine-sdk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  spritekit-spine-sdk
+//
+//  Created by Thiago Medeiros dos Santos on 12/19/15.
+//  Copyright © 2015 Thiago Medeiros dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ spritekit-spine-sdkTests/spritekit_spine_sdkTests.swift
@@ -0,0 +1,36 @@
+//
+//  spritekit_spine_sdkTests.swift
+//  spritekit-spine-sdkTests
+//
+//  Created by Thiago Medeiros dos Santos on 12/19/15.
+//  Copyright © 2015 Thiago Medeiros dos Santos. All rights reserved.
+//
+
+import XCTest
+@testable import spritekit_spine_sdk
+
+class spritekit_spine_sdkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
